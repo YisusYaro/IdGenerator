7f126b5d70e07e5ede2043dc975b91c7c7e5824e
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-10-10 00:16:14 -0500

    feat: remove generic components

--- modules/resources/application/commands/create-resource.interface.ts
+++
@@ -1,7 +0,0 @@
-import { CommandHandler } from '../../../shared/application/commands/command.handler';
-import { CreateResourceCommand } from './create-resource.command';
-
-export interface CreateResourceHandler
-  extends CommandHandler<CreateResourceCommand> {
-  handle(command: CreateResourceCommand): Promise<void>;
-}
