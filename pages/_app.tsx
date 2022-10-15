b28ac06c1c8ed8d1e99109e2736c5b7bee636c0d
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-10-15 11:30:28 -0500

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,29 @@ import "reflect-metadata";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { App } from "../modules/shared/infrastructure/dependency-injection/app";
+import Script from "next/script";
 
 App.getInstance().setDependencyInjectionApp();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PP2DW81169"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-PP2DW81169');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
