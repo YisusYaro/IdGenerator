732f04acf652ebb1ac541979fb620d08645d1eb6
YisusYaro <YisusYaro@users.noreply.github.com>
2022-10-15 16:54:16 +0000

    deploy: 53de8fb9f35bae9cbcf05eb1fd67a8675dddc175

--- _next/static/YsktqzYpYAANydTt_OisY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a5fc26e23b8e942c.css","static/chunks/pages/index-f5bd028d25501463.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/ya_6WKsPncIDNXMYVx3kC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a5fc26e23b8e942c.css","static/chunks/pages/index-2a0a184ecfc823c0.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
