a5934e243314abd98f540344f20567a4020767f4
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-10-15 11:52:34 -0500

    feat: add id generator component

---
+++ components/IdGenerator.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { getUUID } from "../modules/resources/interface/controller";
+import { getULID } from "../modules/resources/interface/controller";
+import styles from "../styles/Home.module.css";
+
+export const IdGenerator: NextPage = () => {
+  const [uuid, setUUID] = useState("");
+  const [ulid, setULID] = useState("");
+
+  useEffect(() => {
+    handleGetUUID();
+    handleGetULID();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleGetUUID = async () => {
+    const { uuid } = await getUUID();
+    setUUID(uuid);
+  };
+
+  const handleGetULID = async () => {
+    const { ulid } = await getULID();
+    setULID(ulid);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>ID Generator</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.box}>
+          <button className={styles.button} onClick={handleGetUUID}>Generate UUID</button>
+          <div className={styles.result}>{uuid}</div>
+        </div>
+        <div className={styles.box}>
+          <button className={styles.button} onClick={handleGetULID}>Generate ULID</button>
+          <div className={styles.result}>{ulid}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
